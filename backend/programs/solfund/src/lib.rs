e719fd49a3aebfe598475fe0afcb814acd77aeec
Success Aje <successaje7@gmail.com>
2024-03-04 21:42:15 +0100

    changes

--- backend/programs/solfund/src/lib.rs
+++ backend/programs/solfund/src/lib.rs
@@ -7,7 +7,7 @@ use anchor_spl::metadata::{
     CreateMetadataAccountsV3, Metadata,
 };
 use anchor_spl::token::{mint_to, Mint, MintTo, Token, TokenAccount};
-use mpl_token_metadata::state::{Collection, Creator, DataV2};
+use mpl_token_metadata::{types::Collection, types::Creator, types::DataV2};
 
 declare_id!("C6i6SQDkP8nbL3ie1PacW2t3Z62zsrbbJE5YiJR7D9u8");
 
@@ -254,4 +254,12 @@ pub struct Campaign {
 	pub donation_complete : bool,
 	pub description: String,
 	pub amount_donated: u64,
+}
+
+#[account]
+pub struct Organization {
+    pub id : String,
+    pub pk : u8,
+    pub name : String,
+
 }
